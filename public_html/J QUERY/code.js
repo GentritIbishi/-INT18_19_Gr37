f0bc1b2530d313458cb26070ec58200ba0eedaa1
EldinVeliji <44035030+EldinVeliji@users.noreply.github.com>
2018-12-23 23:31:09 +0100

    Update code.js

--- public_html/J QUERY/code.js
+++ public_html/J QUERY/code.js
@@ -1,3 +1,4 @@
+//kodi per Social Bar
 var colors = ['1abc9c', '2c3e50', '2980b9', '7f8c8d', 'f1c40f', 'd35400', '27ae60'];
 
 colors.each(function (color) {
@@ -15,7 +16,7 @@ $$('.color-picker')[0].on('click', '.square', function(event, square) {
 
 
 
-
+//Kodi per Hederin
 ;(function( window, document, undefined ) {
 
     'use strict';
@@ -143,94 +144,4 @@ $$('.color-picker')[0].on('click', '.square', function(event, square) {
   // Instantiate HeadsUp
   new headsUp( '.main-header' );
 
-  /*Sliding Images*/
-  $('.slider').each(function() {
-    var $this = $(this);
-    var $group = $this.find('.slide_group');
-    var $slides = $this.find('.slide');
-    var bulletArray = [];
-    var currentIndex = 0;
-    var timeout;
-    
-    function move(newIndex) {
-      var animateLeft, slideLeft;
-      
-      advance();
-      
-      if ($group.is(':animated') || currentIndex === newIndex) {
-        return;
-      }
-      
-      bulletArray[currentIndex].removeClass('active');
-      bulletArray[newIndex].addClass('active');
-      
-      if (newIndex > currentIndex) {
-        slideLeft = '100%';
-        animateLeft = '-100%';
-      } else {
-        slideLeft = '-100%';
-        animateLeft = '100%';
-      }
-      
-      $slides.eq(newIndex).css({
-        display: 'block',
-        left: slideLeft
-      });
-      $group.animate({
-        left: animateLeft
-      }, function() {
-        $slides.eq(currentIndex).css({
-          display: 'none'
-        });
-        $slides.eq(newIndex).css({
-          left: 0
-        });
-        $group.css({
-          left: 0
-        });
-        currentIndex = newIndex;
-      });
-    }
-    
-    function advance() {
-      clearTimeout(timeout);
-      timeout = setTimeout(function() {
-        if (currentIndex < ($slides.length - 1)) {
-          move(currentIndex + 1);
-        } else {
-          move(0);
-        }
-      }, 4000);
-    }
-    
-    $('.next_btn').on('click', function() {
-      if (currentIndex < ($slides.length - 1)) {
-        move(currentIndex + 1);
-      } else {
-        move(0);
-      }
-    });
-    
-    $('.previous_btn').on('click', function() {
-      if (currentIndex !== 0) {
-        move(currentIndex - 1);
-      } else {
-        move(3);
-      }
-    });
-    
-    $.each($slides, function(index) {
-      var $button = $('<a class="slide_btn">&bull;</a>');
-      
-      if (index === currentIndex) {
-        $button.addClass('active');
-      }
-      $button.on('click', function() {
-        move(index);
-      }).appendTo('.slide_buttons');
-      bulletArray.push($button);
-    });
-    
-    advance();
-  });
-  /*sliding images*/
+ 
